d518c739471b7e96505d2fa6063a38d92362ebeb
dm163120m <dm163120m@student.etf.bg.ac.rs>
2017-06-04 12:40:03 +0200

    Task Request missed fix

---
+++ laravel/app/Http/Requests/Task/TaskRequest.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Http\Requests\Task;
+use App\Http\Requests\Request;
+class TaskRequest extends Request
+{
+    public function authorize()
+    {
+        return true;
+    }
+    public function rules(){
+        return [
+            'title' => 'required',
+            'description' => 'required',
+            'assignee' => 'required',
+        ];
+    }
+}
