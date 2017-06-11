fbcd6bf79a3058358ba8ab7791c2aa1597da6db0
DraganaSpasic <sd163256m@student.etf.bg.ac.rs>
2017-06-11 11:17:04 +0200

    Search missing file

--- laravel/app/Http/Controllers/SeniorController.php
+++ laravel/app/Http/Controllers/SeniorController.php
@@ -14,7 +14,8 @@ use App\Task;
 use App\Tasks_Tests;
 use Illuminate\Http\Request;
 
-class SeniorController extends Controller{
+class SeniorController extends Controller
+{
 
     /**
      * @var TaskRepo
@@ -23,13 +24,15 @@ class SeniorController extends Controller{
     private $userRepo;
     private $testRepo;
 
-    public function __construct(TaskRepo $taskRepo, TestRepo $testRepo, UserRepo $userRepo) {
+    public function __construct(TaskRepo $taskRepo, TestRepo $testRepo, UserRepo $userRepo)
+    {
         $this->taskRepo = $taskRepo;
         $this->userRepo = $userRepo;
         $this->testRepo = $testRepo;
     }
 
-    private function getTasksData($data){
+    private function getTasksData($data)
+    {
         $data['tasks'] = $this->taskRepo->getAll();
         $data['juniors'] = $this->userRepo->getJuniors();
         $data['seniors'] = $this->userRepo->getSeniors();
@@ -41,77 +44,86 @@ class SeniorController extends Controller{
         return $data;
     }
 
-    public function tasks(){
+    public function tasks()
+    {
 //        dd($this->taskRepo->getAll());
         $data = $this->userRepo->getUserData();
-        if($data == null) return redirect('login');
+        if ($data == null) return redirect('login');
         $data = $this->getTasksData($data);
         $selected = 0;
-        if(count($data['tasks']) > 0) $selected = $this->taskRepo->getTests(1);
-        return view('senior/tasks')->with('data',$data)->with('selected', $selected);
+        if (count($data['tasks']) > 0) $selected = $this->taskRepo->getTests(1);
+        return view('senior/tasks')->with('data', $data)->with('selected', $selected);
     }
 
-    public function tests(){
+    public function tests()
+    {
         $data = $this->userRepo->getUserData();
-        if($data == null) return redirect('login');
+        if ($data == null) return redirect('login');
         $data = $this->getTasksData($data);
-        return view('senior/allTests')->with('data',$data);
+        return view('senior/allTests')->with('data', $data);
     }
 
-    public function createTask(){
+    public function createTask()
+    {
         $data = $this->userRepo->getUserData();
-        if($data == null) return redirect('login');
+        if ($data == null) return redirect('login');
         $data = $this->getTasksData($data);
-        return view('senior/createTask')->with('data',$data);
+        return view('senior/createTask')->with('data', $data);
     }
 
-    public function getTask($id){
+    public function getTask($id)
+    {
         $data = $this->userRepo->getUserData();
-        if($data == null) return redirect('login');
+        if ($data == null) return redirect('login');
         $data = $this->getTasksData($data);
         $selected = $this->taskRepo->getTests($id);
 //        dd($selected);
-        return view('senior/tasks')->with('data',$data)->with('selected', $selected);
+        return view('senior/tasks')->with('data', $data)->with('selected', $selected);
     }
 
-    public function getTest($id){
+    public function getTest($id)
+    {
         $data = $this->userRepo->getUserData();
-        if($data == null) return redirect('login');
+        if ($data == null) return redirect('login');
         $data = $this->getTasksData($data);
         $selected = $this->testRepo->getById($id);
-        return view('senior/tests')->with('data',$data)->with('selected', $selected);
+        return view('senior/tests')->with('data', $data)->with('selected', $selected);
     }
 
     /**
      * Handle a create task request to the application
      *
-     * @param  \App\Http\Requests\Task\TaskRequest  $request
+     * @param  \App\Http\Requests\Task\TaskRequest $request
      * @return \Illuminate\Http\Response
      */
-    public function createTaskPost(TaskRequest $request){
+    public function createTaskPost(TaskRequest $request)
+    {
         $this->taskRepo->create($request);
         return redirect('/senior/tasks');
     }
 
-    public function updateTask(TaskRequest $request){
+    public function updateTask(TaskRequest $request)
+    {
 //        dd($request->all());
         $this->taskRepo->update($request);
         return redirect('/senior/tasks');
     }
 
-    public function createTest(){
+    public function createTest()
+    {
         $data = $this->userRepo->getUserData();
         $data = $this->getTasksData($data);
-        return view('senior/createTest')->with('data',$data);
+        return view('senior/createTest')->with('data', $data);
     }
 
     /**
      * Handle a create task request to the application
      *
-     * @param  \App\Http\Requests\Test\TestRequest  $request
+     * @param  \App\Http\Requests\Test\TestRequest $request
      * @return \Illuminate\Http\Response
      */
-    public function createTestPost(TestRequest $request){
+    public function createTestPost(TestRequest $request)
+    {
         //dd($request);
         $this->testRepo->create($request);
         return redirect('/senior/tests');
@@ -120,7 +132,7 @@ class SeniorController extends Controller{
     public function notifications(){
         $data = $this->userRepo->getUserData();
         $data["notifications"] = Notification::all();
-        return view('user/home')->with('data',$data);
+        return view('user/home')->with('data', $data);
     }
 
     public function getTests(Request $request){
@@ -135,8 +147,17 @@ class SeniorController extends Controller{
         $data = $this->getTasksData($data);
         $r = $request->all();
         $results = $this->taskRepo->search($r);
-        return view('senior/searchTasks')->with('results',$results)->with('data',$data);
+        return view('senior/searchTasks')->with('results', $results)->with('data', $data);
     }
 
+    public function searchTests(Request $request)
+    {
+        $data = $this->getTasksData([]);
+        $r = $request->all();
+        //dd($r);
+        $results = $this->testRepo->search($r);
+        //dd($results);
 
+        return view('user/searchTests')->with('results', $results)->with('data', $data);
+    }
 }
